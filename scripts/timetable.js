5d94f013e9ba551e6c6baa68e32068f2c78e8ff1
John2T <johntu537@gmail.com>
2022-11-13 21:12:54 -0800

    All 45 course info is done

--- scripts/timetable.js
+++ scripts/timetable.js
@@ -1,3 +1,389 @@
+//Write courses start
+function writeCourses() {
+  //define a variable for the collection you want to create in Firestore to populate data
+  var courseRef = db.collection("courses");
+
+  //Monday start
+  //830-930
+  courseRef.add({
+    name: "Break",
+    location: "",
+    type: "",
+    slot: "m0",
+  });  
+
+  //930-1030
+  courseRef.add({
+    name: "Break",
+    location: "",
+    type: "",
+    slot: "m1",
+  });  
+
+   //1030-1130
+   courseRef.add({
+    name: "Comp1510",
+    location: "SW05-1840",
+    type: "Lecture",
+    slot: "m2",
+  });  
+
+   //1130-1230
+   courseRef.add({
+    name: "Break",
+    location: "",
+    type: "",
+    slot: "m3",
+  });  
+
+   //Comp 1712 1230-1430
+   courseRef.add({
+    name: "Comp 1712",
+    location: "SE12-306",
+    type: "Lab",
+    slot: "m4",
+  });  
+
+   //Comp 1712 1230-1430
+   courseRef.add({
+    name: "Comp 1712",
+    location: "Se12-306",
+    type: "Lab",
+    slot: "m5",
+  });  
+
+   //1430-1530
+   courseRef.add({
+    name: "Break",
+    location: "",
+    type: "",
+    slot: "m6",
+  });  
+
+   //1530-1630
+   courseRef.add({
+    name: "Break",
+    location: "",
+    type: "",
+    slot: "m7",
+  });  
+
+  //1630-1730
+  courseRef.add({
+    name: "Break",
+    location: "",
+    type: "",
+    slot: "m8",
+  });  
+ //Monday end
+
+
+  //Tuesday
+  //COmp 1510 830-1030
+  courseRef.add({
+
+    name: "Comp 1510",
+    location: "SE12-321",
+    type: "Lab",
+    slot: "t0",
+  });  
+
+  courseRef.add({
+    name: "Comp 1510",
+    location: "SE12-321",
+    type: "Lab",
+    slot: "t1",
+  });  
+
+  //comp 1800 1030-1230
+  courseRef.add({
+    name: "Comp 1800",
+    location: "SW05-1710",
+    type: "Lecture",
+    slot: "t2",
+  });  
+
+  courseRef.add({
+    name: "Comp 1800",
+    location: "SW05-1710",
+    type: "Lecture",
+    slot: "t3",
+  });  
+
+  //Break 1 hr
+  courseRef.add({
+    name: "Break",
+    location: "",
+    type: "",
+    slot: "t4",
+  });  
+
+  //Comp 1537 1330-1530
+  courseRef.add({
+    name: "Comp 1537",
+    location: "SW05-1850",
+    type: "Lecture",
+    slot: "t5",
+  })
+ 
+  courseRef.add({
+    name: "Comp 1537",
+    location: "SW05-1850",
+    type: "Lecture",
+    slot: "t6",
+  })
+
+  //Comp 1510 1530-1630
+  courseRef.add({
+    name: "Comp 1510",
+    location: "SW05-1850",
+    type: "Lecture",
+    slot: "t7",
+  })
+
+  //Comp 1100 1630-1730
+  courseRef.add({
+    name: "Comp 1100",
+    location: "SW05-1840",
+    type: "Lecture",
+    slot: "t8",
+  })
+
+  //Tuesday End
+
+
+
+  //Wednesday start
+  //Comp 1510 830-930
+  courseRef.add({
+    name: "Comp 1510",
+    location: "SE12-321",
+    type: "Tutorial",
+    slot: "w0",
+  });  
+
+  //930-1030
+  courseRef.add({
+    name: "Break",
+    location: "",
+    type: "",
+    slot: "w1",
+  });  
+
+  //Comp 1113 1030-1230
+  courseRef.add({
+    name: "Comp 1113",
+    location: "SW05-2875",
+    type: "Lab",
+    slot: "w2",
+  });  
+
+   courseRef.add({
+    name: "Comp 1113",
+    location: "SW05-2875",
+    type: "Lab",
+    slot: "w3",
+  }); 
+  
+  //Comp1510 1230-1330
+  courseRef.add({
+    name: "Comp 1510",
+    location: "SE12-320",
+    type: "Lab",
+    slot: "w4",
+  }); 
+
+  //Comp1510 1330-1430
+  courseRef.add({
+    name: "Comp 1510",
+    location: "SW05-1850",
+    type: "Lecture",
+    slot: "w5",
+  }); 
+
+   //1430-1530
+   courseRef.add({
+    name: "Break",
+    location: "",
+    type: "",
+    slot: "w6",
+  }); 
+
+   //1530-1630
+   courseRef.add({
+    name: "Break",
+    location: "",
+    type: "",
+    slot: "w7",
+  }); 
+
+   //1630-1730
+   courseRef.add({
+    name: "Break",
+    location: "",
+    type: "",
+    slot: "w8",
+  }); 
+  //Wednesday End
+
+
+
+  //Thursday Start
+
+  //COmp 1800 830-1030
+  courseRef.add({
+     name: "Comp 1800",
+    location: "SE12-322",
+     type: "Lab",
+     slot: "th0",
+  });
+
+  courseRef.add({
+    name: "Comp 1800",
+   location: "SE12-322",
+    type: "Lab",
+    slot: "th1",
+ });
+
+  //Comp 1116 1030-1230
+  courseRef.add({
+    name: "Comp 1116",
+    location: "SE12-313",
+    type: "Lab",
+    slot: "th3",
+  });
+
+ 
+ courseRef.add({
+  name: "Comp 1116",
+  location: "SE12-313",
+  type: "Lab",
+  slot: "th4",
+ });
+
+ //break 1hr
+ courseRef.add({
+  name: "Break",
+  location: "",
+  type: "",
+  slot: "th5",
+});  
+
+  //Comp 1537 1330-1530
+ courseRef.add({
+    name: "Comp 1537",
+    location: "SE12-322",
+    type: "Lab",
+    slot: "th6",
+ }); 
+
+ 
+ courseRef.add({
+  name: "Comp 1537",
+  location: "SE12-322",
+  type: "Lab",
+  slot: "th7",
+}); 
+ 
+
+courseRef.add({
+  name: "Break",
+  location: "",
+  type: "",
+  slot: "th7",
+});  
+
+courseRef.add({
+  name: "Break",
+  location: "",
+  type: "",
+  slot: "th8",
+});  
+
+//Thursday end
+
+//Friday start
+//Comp 1116 830-1030
+courseRef.add({
+  name: "Comp 1116",
+  location: "SW05-1840",
+  type: "Lecture",
+  slot: "f0",
+}); 
+
+courseRef.add({
+  name: "Comp 1116",
+  location: "SW05-1840",
+  type: "Lecture",
+  slot: "f1",
+}); 
+
+//Comp1712 1030-1230
+courseRef.add({
+  name: "Comp 1712",
+  location: "NE01-331",
+  type: "Lecture",
+  slot: "f2",
+}); 
+
+
+courseRef.add({
+  name: "Comp 1712",
+  location: "NE01-331",
+  type: "Lecture",
+  slot: "f3",
+}); 
+
+//1230-1330
+courseRef.add({
+  name: "Break",
+  location: "",
+  type: "",
+  slot: "f4",
+}); 
+
+//1330-1430
+courseRef.add({
+  name: "Break",
+  location: "",
+  type: "",
+  slot: "f5",
+}); 
+
+//Comp 1113 1430-1630
+courseRef.add({
+  name: "Comp 1113",
+  location: "SW01-1205",
+  type: "Lecture",
+  slot: "f6",
+}); 
+
+courseRef.add({
+  name: "Comp 1113",
+  location: "SW01-1205",
+  type: "Lecture",
+  slot: "f7",
+}); 
+
+//1630-1730
+courseRef.add({
+  name: "Break",
+  location: "",
+  type: "",
+  slot: "f8",
+}); 
+//Friday End
+}
+//Leaving this code here in case we need to make any changes
+//Keep commented or else you will keep adding courses to database
+//writeCourses();
+//WriteCourses end
+
+
+function readCourses() {
+dbcolelction("courses").doc
+}
+
 
 function readSchedule() {
     db.collection("schedules").doc("set D")                                                      //name of the collection and documents should matach excatly with what you have in Firestore
@@ -59,196 +445,18 @@ function readSchedule() {
           // }
       
       })
-  }
-  
-  readSchedule();
-  
-  // var itemsRef = firebase.database().ref().child("Items");
-  // var tableBody = document.querySelector('#table tbody');
-  
-  // function readData() {
-  //     itemsRef.once('value', function (snapshot) {
-  //         snapshot.forEach(function (item_snapshot) {
-  //             //Create html elements, TRs, TD,s etc.
-  //             var row = document.createElement("tr");
-  //             var col1 = document.createElement("td");
-  //             var col2 = document.createElement("td");
-  //             var col3 = document.createElement("td");
-  //             var col4 = document.createElement("td");
-  //             //Add data to the new elements.
-  //             col1.innerText = item_snapshot.key;
-  //             col2.innerText = item_snapshot.child("First column").val();
-  //             col3.innerText = item_snapshot.child("Second_column").val();
-  //             col4.innerText = item_snapshot.child("Third_column").val();
-  //             //Append the cells into the row and the row into the table body.
-  //             row.appendChild(col1);
-  //             row.appendChild(col2);
-  //             row.appendChild(col3);
-  //             row.appendChild(col4);
-  //             tableBody.appendChild(row);
-  //         });
-  //     });
-  // }
+}
+readSchedule();        //calling the function
 
-
-  //Write courses start
-function writeCourses() {
-    //define a variable for the collection you want to create in Firestore to populate data
-    var courseRef = db.collection("courses");
-  
-  
-  //Currently for tuesday and thursday
-    //Tuesday
-    //COmp 1510 830-1030
-    courseRef.add({
-  
-      name: "Comp 1510",
-      location: "SE12-321",
-      type: "Lab",
-      slot: "t0",
-    });  
-  
-    courseRef.add({
-      name: "Comp 1510",
-      location: "SE12-321",
-      type: "Lab",
-      slot: "t1",
-    });  
-  
-    //comp 1800 1030-1230
-    courseRef.add({
-      name: "Comp 1800",
-      location: "SW05-1710",
-      type: "Lecture",
-      slot: "t2",
-    });  
-  
-    courseRef.add({
-      name: "Comp 1800",
-      location: "SW05-1710",
-      type: "Lecture",
-      slot: "t3",
-    });  
-  
-    //Break 1 hr
-    courseRef.add({
-      name: "Break",
-      location: "",
-      type: "",
-      slot: "t4",
-    });  
-  
-    //Comp 1537 1330-1530
-    courseRef.add({
-      name: "Comp 1537",
-      location: "SW05-1850",
-      type: "Lecture",
-      slot: "t5",
-    })
-   
-    courseRef.add({
-      name: "Comp 1537",
-      location: "SW05-1850",
-      type: "Lecture",
-      slot: "t6",
-    })
-  
-    //Comp 1510 1530-1630
-    courseRef.add({
-      name: "Comp 1510",
-      location: "SW05-1850",
-      type: "Lecture",
-      slot: "t7",
-    })
-  
-    //Comp 1100 1630-1730
-    courseRef.add({
-      name: "Comp 1100",
-      location: "SW05-1840",
-      type: "Lecture",
-      slot: "t8",
-    })
-  
-    //Tuesday End
-  
-  
-    //Thursday Start
-  
-    //COmp 1800 830-1030
-    courseRef.add({
-       name: "Comp 1800",
-      location: "SE12-322",
-       type: "Lab",
-       slot: "th0",
-    });
-  
-    courseRef.add({
-      name: "Comp 1800",
-     location: "SE12-322",
-      type: "Lab",
-      slot: "th1",
-   });
-  
-    //Comp 1116 1030-1230
-    courseRef.add({
-      name: "Comp 1116",
-      location: "SE12-313",
-      type: "Lab",
-      slot: "th3",
-    });
-  
-   
-   courseRef.add({
-    name: "Comp 1116",
-    location: "SE12-313",
-    type: "Lab",
-    slot: "th4",
-   });
-  
-   //break 1hr
-   courseRef.add({
-    name: "Break",
-    location: "",
-    type: "",
-    slot: "th5",
-  });  
-  
-    //Comp 1537 1330-1530
-   courseRef.add({
-      name: "Comp 1537",
-      location: "SE12-322",
-      type: "Lab",
-      slot: "th6",
-   }); 
-  
-   
-   courseRef.add({
-    name: "Comp 1537",
-    location: "SE12-322",
-    type: "Lab",
-    slot: "th7",
-  }); 
-   
-  
-  courseRef.add({
-    name: "Break",
-    location: "",
-    type: "",
-    slot: "th7",
-  });  
-  
-  courseRef.add({
-    name: "Break",
-    location: "",
-    type: "",
-    slot: "th8",
-  });  
-  
-  //Thursday end
-  }
-  //Keep commented or else you will keep adding courses
-  //writeCourses();
-  //WriteCourses end
+//-----------------------------------------------
+// Create a "max" number of hike document objects
+//-----------------------------------------------
+var docData = {
+     Amon: [COMP1510, true, "hello"],
+ };
+ db.collection("timetable").doc("setD").set(docData).then(() => {
+     console.log("Document successfully written!");
+ });
 
 
 
