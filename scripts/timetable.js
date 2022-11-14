b3cdd4df6a1716d7835b657dc19483689e7f9982
Yeju Jung <jungyj320@gmail.com>
2022-11-14 15:41:59 -0800

    timetable.js for loop is made

--- scripts/timetable.js
+++ scripts/timetable.js
@@ -1,62 +1,84 @@
 
 function readSchedule() {
-    db.collection("schedules").doc("set D")                                                      //name of the collection and documents should matach excatly with what you have in Firestore
+    // db.collection("schedules").doc("FRh9ArJ2wyzmJUWky0Vy")
+    //     .onSnapshot(setD => {
+
+
+    // }
+    db.collection("schedules").doc("FRh9ArJ2wyzmJUWky0Vy")                                                      //name of the collection and documents should matach excatly with what you have in Firestore
       .onSnapshot(timetable => {                                                               //arrow notation
-          console.log("current document data: " + timetable.data());                          //.data() returns data object
-          document.getElementById("m0").innerHTML = timetable.data().mon[0];    //using javascript to display the data on the right place
-          document.getElementById("m1").innerHTML = timetable.data().mon[1];
-          document.getElementById("m2").innerHTML = timetable.data().mon[2];
-          document.getElementById("m3").innerHTML = timetable.data().mon[3];
-          document.getElementById("m4").innerHTML = timetable.data().mon[4];
-          document.getElementById("m5").innerHTML = timetable.data().mon[5];
-          document.getElementById("m6").innerHTML = timetable.data().mon[6];
-          document.getElementById("m7").innerHTML = timetable.data().mon[7];
-          document.getElementById("m8").innerHTML = timetable.data().mon[8];
+          console.log("current document data: " + timetable.data());    //.data() returns data object
+          
+        //   for (let i = 0; i < 9; i++) {
+        //       document.getElementById("m"+i).innerHTML = timetable.data().mon[i];
+        //   }
+
+          for (let i = 0; i < 9; i++) {
+            document.getElementById("t"+i).innerHTML = timetable.data().tue[i];
+        }
+
+        for (let i = 0; i < 9; i++) {
+            document.getElementById("w"+i).innerHTML = timetable.data().wed[i];
+        }
+
+        for (let i = 0; i < 9; i++) {
+            document.getElementById("th"+i).innerHTML = timetable.data().thu[i];
+        }
+
+        for (let i = 0; i < 9; i++) {
+            document.getElementById("f"+i).innerHTML = timetable.data().fri[i]; 
+        }
+        //   document.getElementById("m0").innerHTML = timetable.data().mon[0];    //using javascript to display the data on the right place
+        //   document.getElementById("m1").innerHTML = timetable.data().mon[1];
+        //   document.getElementById("m2").innerHTML = timetable.data().mon[2];
+        //   document.getElementById("m3").innerHTML = timetable.data().mon[3];
+        //   document.getElementById("m4").innerHTML = timetable.data().mon[4];
+        //   document.getElementById("m5").innerHTML = timetable.data().mon[5];
+        //   document.getElementById("m6").innerHTML = timetable.data().mon[6];
+        //   document.getElementById("m7").innerHTML = timetable.data().mon[7];
+        //   document.getElementById("m8").innerHTML = timetable.data().mon[8];
   
-          document.getElementById("t0").innerHTML = timetable.data().tue[0];
-          document.getElementById("t1").innerHTML = timetable.data().tue[1];
-          document.getElementById("t2").innerHTML = timetable.data().tue[2];
-          document.getElementById("t3").innerHTML = timetable.data().tue[3];
-          document.getElementById("t4").innerHTML = timetable.data().tue[4];
-          document.getElementById("t5").innerHTML = timetable.data().tue[5];
-          document.getElementById("t6").innerHTML = timetable.data().tue[6];
-          document.getElementById("t7").innerHTML = timetable.data().tue[7];
-          document.getElementById("t8").innerHTML = timetable.data().tue[8];
+        //   document.getElementById("t0").innerHTML = timetable.data().tue[0];
+        //   document.getElementById("t1").innerHTML = timetable.data().tue[1];
+        //   document.getElementById("t2").innerHTML = timetable.data().tue[2];
+        //   document.getElementById("t3").innerHTML = timetable.data().tue[3];
+        //   document.getElementById("t4").innerHTML = timetable.data().tue[4];
+        //   document.getElementById("t5").innerHTML = timetable.data().tue[5];
+        //   document.getElementById("t6").innerHTML = timetable.data().tue[6];
+        //   document.getElementById("t7").innerHTML = timetable.data().tue[7];
+        //   document.getElementById("t8").innerHTML = timetable.data().tue[8];
   
-          document.getElementById("w0").innerHTML = timetable.data().wed[0];   // From wednesday, we don't have schedule data yet. 
-          document.getElementById("w1").innerHTML = timetable.data().wed[1];   // tue[0]; needs to be 
-          document.getElementById("w2").innerHTML = timetable.data().wed[2];
-          document.getElementById("w3").innerHTML = timetable.data().wed[3];
-          document.getElementById("w4").innerHTML = timetable.data().wed[4];
-          document.getElementById("w5").innerHTML = timetable.data().wed[5];
-          document.getElementById("w6").innerHTML = timetable.data().wed[6];
-          document.getElementById("w7").innerHTML = timetable.data().wed[7];
-          document.getElementById("w8").innerHTML = timetable.data().wed[8];
+        //   document.getElementById("w0").innerHTML = timetable.data().wed[0];   // From wednesday, we don't have schedule data yet. 
+        //   document.getElementById("w1").innerHTML = timetable.data().wed[1];   // tue[0]; needs to be 
+        //   document.getElementById("w2").innerHTML = timetable.data().wed[2];
+        //   document.getElementById("w3").innerHTML = timetable.data().wed[3];
+        //   document.getElementById("w4").innerHTML = timetable.data().wed[4];
+        //   document.getElementById("w5").innerHTML = timetable.data().wed[5];
+        //   document.getElementById("w6").innerHTML = timetable.data().wed[6];
+        //   document.getElementById("w7").innerHTML = timetable.data().wed[7];
+        //   document.getElementById("w8").innerHTML = timetable.data().wed[8];
   
-          document.getElementById("th0").innerHTML = timetable.data().thu[0];
-          document.getElementById("th1").innerHTML = timetable.data().thu[1];
-          document.getElementById("th2").innerHTML = timetable.data().thu[2];
-          document.getElementById("th3").innerHTML = timetable.data().thu[3];
-          document.getElementById("th4").innerHTML = timetable.data().thu[4];
-          document.getElementById("th5").innerHTML = timetable.data().thu[5];
-          document.getElementById("th6").innerHTML = timetable.data().thu[6];
-          document.getElementById("th7").innerHTML = timetable.data().thu[7];
-          document.getElementById("th8").innerHTML = timetable.data().thu[8];
+        //   document.getElementById("th0").innerHTML = timetable.data().thu[0];
+        //   document.getElementById("th1").innerHTML = timetable.data().thu[1];
+        //   document.getElementById("th2").innerHTML = timetable.data().thu[2];
+        //   document.getElementById("th3").innerHTML = timetable.data().thu[3];
+        //   document.getElementById("th4").innerHTML = timetable.data().thu[4];
+        //   document.getElementById("th5").innerHTML = timetable.data().thu[5];
+        //   document.getElementById("th6").innerHTML = timetable.data().thu[6];
+        //   document.getElementById("th7").innerHTML = timetable.data().thu[7];
+        //   document.getElementById("th8").innerHTML = timetable.data().thu[8];
   
-          document.getElementById("f0").innerHTML = timetable.data().fri[0];
-          document.getElementById("f1").innerHTML = timetable.data().fri[1];
-          document.getElementById("f2").innerHTML = timetable.data().fri[2];
-          document.getElementById("f3").innerHTML = timetable.data().fri[3];
-          document.getElementById("f4").innerHTML = timetable.data().fri[4];
-          document.getElementById("f5").innerHTML = timetable.data().fri[5];
-          document.getElementById("f6").innerHTML = timetable.data().fri[6];
-          document.getElementById("f7").innerHTML = timetable.data().fri[7];
-          document.getElementById("f8").innerHTML = timetable.data().fri[8];
+        //   document.getElementById("f0").innerHTML = timetable.data().fri[0];
+        //   document.getElementById("f1").innerHTML = timetable.data().fri[1];
+        //   document.getElementById("f2").innerHTML = timetable.data().fri[2];
+        //   document.getElementById("f3").innerHTML = timetable.data().fri[3];
+        //   document.getElementById("f4").innerHTML = timetable.data().fri[4];
+        //   document.getElementById("f5").innerHTML = timetable.data().fri[5];
+        //   document.getElementById("f6").innerHTML = timetable.data().fri[6];
+        //   document.getElementById("f7").innerHTML = timetable.data().fri[7];
+        //   document.getElementById("f8").innerHTML = timetable.data().fri[8];
   
-          // for (let i = 0; i < mon.length; i++) {
-          //     document.querySelector('[id^="m"+i]').id.innerHTML = monday.data().mon[i];
-          //    // document.getElementById("m" + i).innerHTML = monday.data().mon[i]; 
-          // }
+          
       
       })
   }
